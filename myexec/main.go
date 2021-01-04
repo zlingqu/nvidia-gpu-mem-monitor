79e5bf2d48dfb2c456fe3cba6e3ea519ad71fc16
quzhongling <zlingqu@126.com>
2021-01-04 16:42:20 +0800

    更新myexec

--- myexec/main.go
+++ myexec/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/csv"
 	"fmt"
+	"io/ioutil"
 	"os/exec"
 )
 
@@ -24,12 +25,31 @@ func GetExecOutByCSV(argu string) [][]string {
 func GetExecOutByString(argu string) string {
 
 	cmd := exec.Command("/bin/bash", "-c", argu)
-	output, err := cmd.Output()
+
+	//创建获取命令输出管道
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		fmt.Printf("Execute Shell:%s failed with error:%s", cmd, err.Error())
-		return ""
+		fmt.Printf("Error:can not obtain stdout pipe for command:%s\n", err)
+		return "error"
 	}
 
-	return string(output)
+	//执行命令
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Error:The command is err,", err)
+		return "error"
+	}
 
+	//读取所有输出
+	myByte, err := ioutil.ReadAll(stdout)
+	if err != nil {
+		fmt.Println("ReadAll Stdout:", err.Error())
+		return "error"
+	}
+
+	if err := cmd.Wait(); err != nil {
+		fmt.Println("wait:", err.Error())
+		return "error"
+	}
+	// fmt.Printf("stdout:\n\n %s", bytes)
+	return string(myByte[0 : len(myByte)-1])
 }
